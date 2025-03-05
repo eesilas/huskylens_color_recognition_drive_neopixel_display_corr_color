7ef750d458dcad17591538673da8001b605bda82
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-03-05 09:46:21 +0800

    revised

--- main.ts
+++ main.ts
@@ -1,18 +1,16 @@
-let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
+let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
 basic.forever(function () {
     huskylens.request()
-    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        } else if (huskylens.isAppear(4, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-        } else {
-            basic.showIcon(IconNames.Heart)
-        }
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (huskylens.isAppear(4, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else {
+        basic.showIcon(IconNames.Heart)
     }
 })
